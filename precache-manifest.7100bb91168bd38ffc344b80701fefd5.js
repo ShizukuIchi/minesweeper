e9633abc92d6e7c5733c2da352d9d37086a6f92a
ShizukuIchi <shizukuichi@gmail.com>
2019-02-24 08:20:24 +0800

    Updates

---
+++ precache-manifest.7100bb91168bd38ffc344b80701fefd5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ffe58712ac7260b0046e",
+    "url": "/minesweeper/static/js/runtime~main.ffe58712.js"
+  },
+  {
+    "revision": "d254da8ceb8bcb4dedfe",
+    "url": "/minesweeper/static/js/main.d254da8c.chunk.js"
+  },
+  {
+    "revision": "c4e7ee957efd380eff52",
+    "url": "/minesweeper/static/js/2.c4e7ee95.chunk.js"
+  },
+  {
+    "revision": "d254da8ceb8bcb4dedfe",
+    "url": "/minesweeper/static/css/main.a3d05263.chunk.css"
+  },
+  {
+    "revision": "7d8da29d04c26f988b3497b369a6098d",
+    "url": "/minesweeper/index.html"
+  }
+];
